f18137e9f90a6e162675070804781bb01874ca59
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-12 10:13:36 +0100

    changes

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -134,7 +134,7 @@ function Options() {
                                 ))}
                             </div>
                         </div>
-                        <div className="flex-col w-auto h-[40vh] place-items-center">
+                        <div className="flex-col w-auto h-[40vh] place-items-center pl-5">
                             <svg xmlns="http://www.w3.org/2000/svg"fill="none" 
                             viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
                             className="size-6 w-24 h-24 fill-green-500 stroke-green-500">
@@ -142,7 +142,7 @@ function Options() {
                             d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 
                             0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                             </svg>
-                            <div className="flex items-center justify-start px-2 w-[60%] font-[500] mt-1 h-8 bg-gray-300">
+                            <div className="flex items-center justify-start px-2 w-[70%] font-[500] mt-1 h-8 bg-gray-300">
                                 {state.username.toUpperCase()}
                             </div>
                             <div className="h-12 w-[40%] mt-3">
