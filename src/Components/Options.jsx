f97ef8dac24ec47559d5ef2f488b71c3b0e5738c
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-12 08:33:57 +0100

    changes

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -96,10 +96,10 @@ function Options() {
                         ))}
                     </div>
                     <div className="grid grid-cols-3 w-full">
-                        <div className="partion w-80 py-5 mx-4 mt-6 h-80 overflow-y-auto">
+                        <div className="partion w-80 mx-4 h-80">
                             <div className="chosenSubjects flex-col">
                                 {state.chosenSubjects.map((item) =>(
-                                    <div key={item.id} className="subject h-[11vh] py-2 w-full bg-green-500 border-b-8 border-x-white flex-col">
+                                    <div key={item.id} className="subject h-[11vh] py-2 w-fit bg-green-500 border-b-8 border-x-white flex-col">
                                         <div className="flex gap-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" 
                                             fill="none" viewBox="0 0 24 24"
